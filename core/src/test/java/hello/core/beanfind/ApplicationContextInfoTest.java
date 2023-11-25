50a8d343857140e06e51c6ccc46d5a09b548d287
wonuk(du_ck) <wonuk007@naver.com>
2023-11-25 15:57:34 +0900

    자체 등록한 빈 및 스프링이 등록한 빈 목록 테스트

---
+++ core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,42 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name : " + beanDefinitionName + " Object : " + bean);
+        }
+    }
+
+
+    /**
+     * 스프링 자체에서 등록하는 bean 이 아닌 내가 추가한 bean 만 출력
+     */
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            //BeanDefinition.ROLE_APPLICATION : 직접 등록한 빈
+            //BeanDefinition.ROLE_INFRASTRUCTURE : 스프링이 등록한 빈
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name : " + beanDefinitionName + " Object : " + bean);
+            }
+        }
+    }
+}
