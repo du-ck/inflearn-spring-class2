69246b59a29676991f899a6916e9b559bc693d47
wonuk(du_ck) <wonuk007@naver.com>
2023-11-30 17:15:12 +0900

    @Component @Autowired 컴포넌트 스캔과 의존관계 자동 주입

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+
+/**
+ * @ComponentScan 을 사용하면 @Configuration 이 붙은 설정 정보도 자동으로 등록되기 때문에
+ * AppConfig, TestConfig 등 앞에서 만든 설정정보들이 함께 등록되고 실행되어 버린다.
+ * 따라서 excludeFilters 를 이용해 해당 설정정보들은 제외한다.
+ */
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+
+
+}
