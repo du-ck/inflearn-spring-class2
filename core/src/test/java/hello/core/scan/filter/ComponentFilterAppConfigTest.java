7ee6b0f7b087de9cd06595defd40a0391f5bc195
wonuk(du_ck) <wonuk007@naver.com>
2023-12-05 19:40:13 +0900

    조회 대상 빈이 2개이상일 케이스

--- core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
+++ core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -19,8 +19,7 @@ public class ComponentFilterAppConfigTest {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
         BeanA beanA = ac.getBean("beanA", BeanA.class);
         assertThat(beanA).isNotNull();
-
-        ac.getBean("beanB", BeanB.class);
+        /*ac.getBean("beanB", BeanB.class);*/
         assertThrows(
                 NoSuchBeanDefinitionException.class,
                 () ->  ac.getBean("beanB", BeanB.class));
