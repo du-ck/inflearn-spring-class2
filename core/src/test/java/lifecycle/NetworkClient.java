8108475a1fa6b00c66a2c1d3a6ad048f65a85cc5
wonuk(du_ck) <wonuk007@naver.com>
2023-12-14 23:43:39 +0900

    networkclient 테스트 수정

--- core/src/test/java/lifecycle/NetworkClient.java
+++ core/src/test/java/lifecycle/NetworkClient.java
@@ -9,7 +9,7 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
-        System.out.println("생성자 호출 , url = " + url);
+        System.out.println("생성자 호출 , url == " + url);
         /*connect();
         call("초기화 연결 메세지");*/
     }
