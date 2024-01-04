64d38fd1bb9b959e81e7ce7b692d466219542662
wonuk(du_ck) <wonuk007@naver.com>
2024-01-04 17:00:42 +0900

    ObjectProvider 사용 및 테스트

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,11 +14,17 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
 
-    @RequestMapping("log-demoo")
+    /**
+     * ObjectProvider 덕분에 호출시점까지 request scope 빈의 생성을 지연할 수 있다.
+     * myLogger 를 통해 동시요청이와도 uuid 값으로 로그 구분 가능
+     */
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
 
